ea66d9f4f40100d033d07f195384cfb1869b6c38
Tobiah <tobiah@protonmail.com>
2019-06-10 13:16:12 -0500

    test using now for wfstatus

---
+++ now.js
@@ -0,0 +1,12 @@
+{
+  "version": 2,
+  "builds": [{
+    "src": "package.json",
+    "use": "@now/node"
+  }],
+  "github": {
+    "silent": true
+  },
+  "name": "warframe-status",
+  "scope": "wfcd"
+}
