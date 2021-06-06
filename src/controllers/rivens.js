9442fe28980cef729680df35e4a5f0d3a6ebbd41
Tobiah <tobiah@pm.me>
2021-06-05 21:24:51 -0500

    chore: use raw cdn link for rivens

--- src/controllers/rivens.js
+++ src/controllers/rivens.js
@@ -39,7 +39,7 @@ const groupRivenData = (cacheStrData) => {
 };
 
 platforms.forEach((platform) => {
-  const rCache = new Cache(`https://www.warframe.com/repos/weeklyRivens${platform.toUpperCase()}.json`, 604800000, {
+  const rCache = new Cache(`https://n9e5v4d8.ssl.hwcdn.net/repos/weeklyRivens${platform.toUpperCase()}.json`, 604800000, {
     parser: groupRivenData,
     logger,
     delayStart: true,
