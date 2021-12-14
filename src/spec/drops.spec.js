862f33fce5c1f88734a9e4d8363820b029945101
AyAyEm <andre20114meira@gmail.com>
2021-12-14 11:33:24 -0300

    Feat: drop search matching tests

--- src/spec/drops.spec.js
+++ src/spec/drops.spec.js
@@ -16,11 +16,57 @@ describe('drops', () => {
     res.body.length.should.be.greaterThan(0);
   });
   describe('search', () => {
-    it('should look up forma', async () => {
-      const res = await chai.request(server).get('/drops/search/forma');
+    describe('by item', () => {
+      it('should look up forma', async () => {
+        const res = await chai.request(server).get('/drops/search/Forma');
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body.length.should.be.greaterThan(0);
+      });
+      it('should be inclusive', async () => {
+        const res = await chai.request(server).get('/drops/search/Form');
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body.length.should.be.greaterThan(0);
+        res.body.map(({ item }) => item).should.include('Forma');
+      });
+      it('should be case insensitive', async () => {
+        const res = await chai.request(server).get('/drops/search/forma');
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body.length.should.be.greaterThan(0);
+        res.body.map(({ item }) => item).should.include('Forma');
+      });
+    });
+    describe('by place', () => {
+      it('should look up hydron', async () => {
+        const res = await chai.request(server).get('/drops/search/Sedna%2FHydron (Defense), Rot A');
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body.length.should.be.greaterThan(0);
+      });
+      it('should be inclusive', async () => {
+        const res = await chai.request(server).get('/drops/search/Hydron');
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body.length.should.be.greaterThan(0);
+        res.body.map(({ place }) => place).should.include('Sedna/Hydron (Defense), Rot A');
+      });
+      it('should be case insensitive', async () => {
+        const res = await chai.request(server).get('/drops/search/sedna%2Fhydron (defense)');
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body.length.should.be.greaterThan(0);
+        res.body.map(({ place }) => place).should.include('Sedna/Hydron (Defense), Rot A');
+      });
+    });
+    it('should search by item and place', async () => {
+      const res = await chai.request(server).get('/drops/search/Hydron,Forma');
       res.should.have.status(200);
       res.body.should.be.an('array');
       res.body.length.should.be.greaterThan(0);
+      res.body.map(({ place }) => place).should.include('Sedna/Hydron (Defense), Rot A');
+      res.body.map(({ item }) => item).should.include('Forma');
     });
     it('should group by location', async () => {
       const res = await chai.request(server).get('/drops/search/forma?grouped_by=location');
