77144f51b336141c4ab22430861bc3a134422f5f
Tobiah <tobiah@pm.me>
2022-05-05 23:40:03 -0500

    add conditional failure cases for wfinfo

--- src/spec/wfinfo.spec.js
+++ src/spec/wfinfo.spec.js
@@ -8,22 +8,39 @@ chai.use(chaiHttp);
 
 describe('wfinfo', () => {
   describe('filtered_items', () => {
-    it('should not be empty', async () => {
-      const res = await chai.request(server)
-        .get('/wfinfo/filtered_items');
-      res.should.have.status(200);
-      res.body.should.be.an('object');
-      Object.keys(res.body).length.should.eq(5);
-      res.body.should.have.keys(['timestamp', 'errors', 'relics', 'eqmt', 'ignored_items']);
-    });
+    if (process.env.WFINFO_FILTERED_ITEMS) {
+      it('should not be empty', async () => {
+        const res = await chai.request(server)
+          .get('/wfinfo/filtered_items');
+        res.should.have.status(200);
+        res.body.should.be.an('object');
+        Object.keys(res.body).length.should.eq(5);
+        res.body.should.have.keys(['timestamp', 'errors', 'relics', 'eqmt', 'ignored_items']);
+      });
+    } else {
+      it('should be unavailable', async () => {
+        const res = await chai.request(server)
+          .get('/wfinfo/filtered_items');
+        res.should.have.status(503);
+      });
+    }
   });
+
   describe('prices', () => {
-    it('should not be empty', async () => {
-      const res = await chai.request(server)
-        .get('/wfinfo/prices');
-      res.should.have.status(200);
-      res.body.should.be.an('array');
-      res.body[0].should.have.keys(['name', 'yesterday_vol', 'today_vol', 'custom_avg']);
-    });
+    if (process.env.WFINFO_PRICES) {
+      it('should not be empty', async () => {
+        const res = await chai.request(server)
+          .get('/wfinfo/prices');
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body[0].should.have.keys(['name', 'yesterday_vol', 'today_vol', 'custom_avg']);
+      });
+    } else {
+      it('should be unavailable', async () => {
+        const res = await chai.request(server)
+          .get('/wfinfo/prices');
+        res.should.have.status(503);
+      });
+    }
   });
 });
