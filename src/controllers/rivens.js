238d004d890197d7c472c8341ec33ef92af95292
Matt <7128721+TobiTenno@users.noreply.github.com>
2023-11-30 10:19:41 -0600

    fix: rivens domain moved

--- src/controllers/rivens.js
+++ src/controllers/rivens.js
@@ -39,7 +39,7 @@ const groupRivenData = (cacheStrData) => {
 
 platforms.forEach((platform) => {
   const rCache = new Cache(
-    `https://n9e5v4d8.ssl.hwcdn.net/repos/weeklyRivens${platform.toUpperCase()}.json`,
+    `https://www.warframe.com/repos/weeklyRivens${platform.toUpperCase()}.json`,
     604800000,
     {
       parser: groupRivenData,
