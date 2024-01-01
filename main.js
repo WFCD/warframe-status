68c215a860273fc11867e52c66be250b0de508de
Matej Voboril <tobiah@pm.me>
2024-01-01 11:13:29 -0600

    remove startup delay

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 'use strict';
 
+const http = require('http');
+
+const app = require('./src/app');
+const makeSocket = require('./src/socket');
+const logger = require('./src/lib/logger')('HTTP');
 const { host, port, features } = require('./src/lib/settings');
 
-// manually delay startup to try to let host populate/resolve
-setTimeout(() => {
-  const http = require('http');
-  const app = require('./src/app');
-  const makeSocket = require('./src/socket');
-  const logger = require('./src/lib/logger')('HTTP');
+const server = http.createServer(app);
+if (features.includes('SOCKET')) makeSocket(server);
 
-  const server = http.createServer(app);
-  if (features.includes('SOCKET')) makeSocket(server);
-
-  logger.info(`Listening to ${host}:${port}`);
-  server.listen(port, host);
-}, 10000);
+logger.info(`Listening to ${host}:${port}`);
+server.listen(port, host);
