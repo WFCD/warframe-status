b30bdc67a89af743615b1d1d7b37a06a5814d364
Matej Voboril <tobiah@pm.me>
2024-01-01 11:12:13 -0600

    resolve dns records, but allow undefined by default
    somehow works better than ipv6

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 'use strict';
 
-const http = require('http');
-const app = require('./src/app');
-const makeSocket = require('./src/socket');
 const { host, port, features } = require('./src/lib/settings');
-const logger = require('./src/lib/logger')('HTTP');
 
-const server = http.createServer(app);
-if (features.includes('SOCKET')) makeSocket(server);
+// manually delay startup to try to let host populate/resolve
+setTimeout(() => {
+  const http = require('http');
+  const app = require('./src/app');
+  const makeSocket = require('./src/socket');
+  const logger = require('./src/lib/logger')('HTTP');
 
-logger.info(`Listening to ${host}:${port}`);
-server.listen(port, host);
+  const server = http.createServer(app);
+  if (features.includes('SOCKET')) makeSocket(server);
+
+  logger.info(`Listening to ${host}:${port}`);
+  server.listen(port, host);
+}, 10000);
