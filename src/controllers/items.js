eaa3c0296c492bb587e1f2345468721f0fa17dd9
Matt <7128721+TobiTenno@users.noreply.github.com>
2024-06-27 23:47:59 -0500

    feat(ws,items): dynamic filters (#1549)

--- src/controllers/items.js
+++ src/controllers/items.js
@@ -4,7 +4,15 @@ import { noResult, trimPlatform, cache, ah } from '../lib/utilities.js';
 import Items from '../lib/caches/Items.js';
 
 const router = express.Router();
-const splitKeys = (input) => (input || '').split(',').filter((k) => k.length);
+const splitKeys = (input) => (input || '').split(',').filter(Boolean);
+const splitFilter = (input) =>
+  (input || '')
+    .split(',')
+    .filter(Boolean)
+    .map((i) => {
+      const [key, value] = i.split(':');
+      return { key, value };
+    });
 
 router.use((req, res, next) => {
   req.itemType = trimPlatform(req.baseUrl);
@@ -43,11 +51,12 @@ router.use((req, res, next) => {
 router.get(
   '/',
   ah(async (req, res) => {
-    const { remove, only } = req.query;
+    const { remove, only, filter } = req.query;
     return res.status(200).json(
       await Items.get(req.itemType, req.language, {
         remove: splitKeys(remove),
         only: splitKeys(only),
+        filter: splitFilter(filter),
       })
     );
   })
@@ -89,11 +98,12 @@ router.get(
   '/:item/?',
   cache('10 hours'),
   ah(async (req, res) => {
-    const { remove, only } = req.query;
+    const { remove, only, filter, by } = req.query;
     const result = await Items.get(req.itemType, req.language, {
-      by: req.query.by,
+      by,
       only: splitKeys(only),
       remove: splitKeys(remove),
+      filter: splitFilter(filter),
       max: 1,
       term: req.params.item,
     });
@@ -141,7 +151,7 @@ router.get(
   '/search/:query/?',
   cache('10 hours'),
   ah(async (req, res) => {
-    const { remove, only, by = 'name' } = req.query;
+    const { remove, only, by = 'name', filter } = req.query;
     const queries = req.params.query
       .trim()
       .split(',')
@@ -155,6 +165,7 @@ router.get(
           only: splitKeys(only),
           term: query,
           max: 0,
+          filter: splitFilter(filter),
         })
       );
     }
