df91daf8105d1f2c9a455c14fa76097f1b626c35
Matej Voboril <tobiah@pm.me>
2024-11-01 15:50:53 -0500

    refactor: twitch & wfinfo standard caches

--- src/controllers/wfinfo.js
+++ src/controllers/wfinfo.js
@@ -1,30 +1,17 @@
-import { resolve, dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 import express from 'express';
-import { create } from 'flat-cache';
-import { CronJob } from 'cron';
 
-import settings from '../lib/settings.js';
+import WFInfo from '../lib/caches/WFInfo.js';
 import { cache, ah } from '../lib/utilities.js';
 
-const dirName = dirname(fileURLToPath(import.meta.url));
 const router = express.Router();
-let infoCache;
-
-router.use((req, res, next) => {
-  if (!infoCache) infoCache = create({ cacheId: '.wfinfo', cacheDir: resolve(dirName, '../../') });
-  next();
-});
 
 router.get(
   '/filtered_items/?',
   cache('1 hour'),
   ah(async (req, res) => {
-    if (settings.wfInfo?.filteredItems) {
-      return res.status(200).json(infoCache.getKey('filteredItems'));
-    }
-    return res.status(503).json({ code: 503, error: 'WFInfo Data Services Unavailable' });
+    return res
+      .status(WFInfo.items ? 200 : 503)
+      .json(WFInfo.items || { code: 503, error: 'WFInfo Data Services Unavailable' });
   })
 );
 
@@ -32,23 +19,10 @@ router.get(
   '/prices/?',
   cache('1 hour'),
   ah(async (req, res) => {
-    if (settings.wfInfo?.prices) {
-      return res.status(200).json(infoCache.getKey('prices'));
-    }
-    return res.status(503).json({ code: 503, error: 'WFInfo Data Services Unavailable' });
+    return res
+      .status(WFInfo.prices ? 200 : 503)
+      .json(WFInfo.prices || { code: 503, error: 'WFInfo Data Services Unavailable' });
   })
-);
-
-// eslint-disable-next-line no-new
-new CronJob(
-  '0 5 * * * *',
-  /* istanbul ignore next */
-  () => {
-    /* istanbul ignore next */
-    infoCache = create({ cacheId: '.wfinfo', cacheDir: resolve(dirName, '../../') });
-  },
-  undefined,
-  true
 );
 
 export default router;
--- src/lib/hydrate.js
+++ src/lib/hydrate.js
@@ -1,87 +1,10 @@
-import { resolve, dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-import { create } from 'flat-cache';
-
 import Logger from './logger.js';
-import { wfInfo, build } from './settings.js';
+import { build } from './settings.js';
 import ItemsCache from './caches/Items.js';
 import RivensCache from './caches/Rivens.js';
 import DropsCache from './caches/Drops.js';
-
-const { filteredItems: filteredItemsSrc, prices: pricesSrc } = wfInfo;
-
-const dirName = dirname(fileURLToPath(import.meta.url));
-
-const TWO_HOURS = 7200000;
-const TWO_DAYS = 172800000;
-
-// TODO: Eventually migrate to a standalone cache class
-const hydrateWfInfo = async (logger) => {
-  const start = Date.now();
-  // WF Info caches
-  const wfInfoCache = create({ cacheId: '.wfinfo', cacheDir: resolve(dirName, '../../') });
-  if (Date.now() - (wfInfoCache.getKey('last_updt') || 0) >= TWO_HOURS / 2) {
-    if (filteredItemsSrc) {
-      const itemsRes = await fetch(filteredItemsSrc);
-      const itemsRaw = await itemsRes.text();
-      try {
-        const d = JSON.parse(itemsRaw);
-        wfInfoCache.setKey('filteredItems', d);
-      } catch (e) {
-        logger.error(`Failed to update wfinfo filtered items`, e);
-      }
-    }
-    if (pricesSrc) {
-      const pricesRes = await fetch(pricesSrc);
-      const pricesRaw = await pricesRes.text();
-      try {
-        const d = JSON.parse(pricesRaw);
-        wfInfoCache.setKey('prices', d);
-      } catch (e) {
-        logger.error(`Failed to update wfinfo Prices`, e);
-      }
-    }
-    wfInfoCache.setKey('last_updt', Date.now());
-    wfInfoCache.save(true);
-
-    const end = Date.now();
-    logger.info(`WFInfo Hydration complete in ${end - start}ms`);
-  }
-};
-
-// TODO: Eventually migrate to a standalone cache class
-const hydrateTwitch = async (logger) => {
-  // Twitch extension token cache
-  const twitchCache = create({ cacheId: '.twitch', cacheDir: resolve(dirName, '../../') });
-  const CLIENT_ID = 'kimne78kx3ncx6brgo4mv6wki5h1ko'; // twitch's client id
-  const WF_ARSENAL_ID = 'ud1zj704c0eb1s553jbkayvqxjft97';
-  const TWITCH_CHANNEL_ID = '89104719'; // tobitenno
-  if (
-    CLIENT_ID &&
-    Date.now() - (twitchCache.getKey('last_updt') || 0) >= TWO_DAYS &&
-    twitchCache.getKey('token') !== 'unset'
-  ) {
-    try {
-      let raw = await fetch(`https://gql.twitch.tv/gql`, {
-        method: 'POST',
-        headers: {
-          'client-id': CLIENT_ID,
-        },
-        body: `[{"operationName":"ExtensionsForChannel","variables":{"channelID":"${TWITCH_CHANNEL_ID}"},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"d52085e5b03d1fc3534aa49de8f5128b2ee0f4e700f79bf3875dcb1c90947ac3"}}}]`,
-      }).then((d) => d.json());
-      raw = raw?.[0]?.data?.user?.channel?.selfInstalledExtensions?.find((s) => {
-        return s?.token?.extensionID === WF_ARSENAL_ID;
-      })?.token?.jwt;
-      raw = raw || 'unset';
-      twitchCache.setKey('token', raw);
-      twitchCache.setKey('last_updt', Date.now());
-      twitchCache.save(true);
-    } catch (e) {
-      logger.error('Cannot hydrate Twitch token');
-    }
-  }
-};
+import WFInfoCache from './caches/WFInfo.js';
+import TwitchCache from './caches/Twitch.js';
 
 const hydrate = async () => {
   const logger = Logger('HYDRATE');
@@ -89,8 +12,8 @@ const hydrate = async () => {
   await ItemsCache.populate();
   await DropsCache.populate();
   await RivensCache.populate();
-  await hydrateWfInfo(logger);
-  await hydrateTwitch(logger);
+  await WFInfoCache.populate(logger);
+  await TwitchCache.populate(logger);
 };
 
 if (build) {
